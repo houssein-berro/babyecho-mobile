d44972dee171eed37af1fe77387309e658d41f3a
Berro-1 <hussienberro1@gmail.com>
2024-08-24 16:13:18 +0300

    Adjusted imports

--- src/components/navigation/mainStackNavigator.jsx
+++ src/components/navigation/mainStackNavigator.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from '../screens/home';
-import DetailsScreen from '../screens/details';
+import HomeScreen from '../../screens/home';
+import DetailsScreen from '../../screens/details';
 
 const Stack = createNativeStackNavigator();
 
