fb62fd051fb9c218c0f3e855ac0fc6bd4225dbd1
Berro-1 <hussienberro1@gmail.com>
2024-08-24 16:15:45 +0300

    Adjusted layout in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import MainStackNavigator from './navigation/mainStackNavigator';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import OnboardingNavigator from './components/navigation/onboardingNavigator';
+// import LoginScreen from './screens/LoginScreen';
+// import SignupScreen from './screens/SignupScreen';
+  import MainStackNavigator from './components/navigation/mainStackNavigator'; // Assuming you have a main app navigator
 
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <MainStackNavigator />
+      <Stack.Navigator initialRouteName="Onboarding">
+        <Stack.Screen
+          name="Onboarding"
+          component={OnboardingNavigator}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Main"
+          component={MainStackNavigator}
+          options={{ headerShown: false }}
+        />
+        {/* <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} /> */}
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
