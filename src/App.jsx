4eb210fe70efe7a105aaf056be3dbf1d0f0c40f8
Berro-1 <hussienberro1@gmail.com>
2024-08-24 12:34:28 +0300

    Adjusted structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './screens/HomeScreen';
-import DetailsScreen from './screens/DetailsScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import MainStackNavigator from './navigation/mainStackNavigator';
 
-const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
+      <MainStackNavigator />
     </NavigationContainer>
   );
 }
