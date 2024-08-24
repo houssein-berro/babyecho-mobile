617c8bbee2ca97d9fd8c36dbceb5071af089e50d
Berro-1 <hussienberro1@gmail.com>
2024-08-24 17:58:44 +0300

    Added signup action

--- src/redux/auth/authActions.js
+++ src/redux/auth/authActions.js
@@ -15,3 +15,15 @@ export const loginUser = (userData) => async (dispatch) => {
     dispatch(loginFailure(error.response?.data?.message || error.message || "Login failed"));
   }
 };
+
+// Signup action creator
+export const signupUser = (userData) => async (dispatch) => {
+  dispatch(signupStart());
+  try {
+    const response = await axios.post(`${process.env.BACKEND_URL}/auth/signup`, userData);
+    dispatch(signupSuccess(response.data));
+    await AsyncStorage.setItem('token', response?.data.token);
+  } catch (error) {
+    dispatch(signupFailure(error.response?.data?.message || error.message || "Signup failed"));
+  }
+};
