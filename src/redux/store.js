480bfff3d7fba2d919db5993471cabcfb4e861bb
Berro-1 <hussienberro1@gmail.com>
2024-08-24 18:03:28 +0300

    Added auth reducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './slices/userSlice';
-
+import authReducer from './auth/authSlice';
 export const store = configureStore({
   reducer: {
-    user: userReducer,
+  auth: authReducer,
   },
 });
 
