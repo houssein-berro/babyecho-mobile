b1530d28eb0c537b0cd899c214529a4ff5d21666
Berro-1 <hussienberro1@gmail.com>
2024-08-24 16:13:44 +0300

    Adjusted styling

--- src/screens/onboarding/onboarding2Screen.jsx
+++ src/screens/onboarding/onboarding2Screen.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import { Text, Image, StyleSheet } from 'react-native';
-import ScreenWrapper from '../components/ScreenWrapper';
-import Button from '../components/Button';
+import ScreenWrapper from '../../components/screenWrapper/screenWrapper';
+import Button from '../../components/button/button';
 
 export default function Onboarding2Screen({ navigation }) {
   return (
     <ScreenWrapper>
       <Text style={styles.title}>Stay Informed Instantly</Text>
       <Image
-        source={require('../assets/onboarding2.png')}
+        source={require('../../assets/public/onboarding2.png')}
         style={styles.image}
       />
       <Text style={styles.subtitle}>Get alerts when your baby needs attention.</Text>
       <Button
         title="Login"
-        onPress={() => navigation.navigate('Login')}
+        onPress={() => navigation.replace('Login')} // Replace to avoid going back to onboarding
       />
       <Button
         title="Signup"
-        onPress={() => navigation.navigate('Signup')}
-        outlined={true} 
+        onPress={() => navigation.replace('Signup')} // Replace to avoid going back to onboarding
+        outlined={true}
         style={styles.signupButton}
       />
     </ScreenWrapper>
@@ -27,23 +27,26 @@ export default function Onboarding2Screen({ navigation }) {
 }
 
 const styles = StyleSheet.create({
+
   title: {
-    fontSize: 24,
+    fontSize: 28,
     fontWeight: 'bold',
-    color: '#333333',
-    marginBottom: 20,
+    width:'80%',
+    color: '#424242',
+    marginBottom: 40,
   },
   image: {
-    width: 250,
+    width: '100%',
     height: 250,
-    marginBottom: 20,
+    marginBottom: 10,
   },
   subtitle: {
-    fontSize: 16,
+    fontSize: 18,
     color: '#666666',
     textAlign: 'center',
-    marginBottom: 30,
+    marginBottom: 60,
   },
+
   signupButton: {
     marginTop: 10,
   },
