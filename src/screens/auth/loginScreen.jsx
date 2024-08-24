6784bc007b933563ce846e4b5a361329af9c4e66
Berro-1 <hussienberro1@gmail.com>
2024-08-24 17:23:33 +0300

    Added styling

--- src/screens/auth/loginScreen.jsx
+++ src/screens/auth/loginScreen.jsx
@@ -20,7 +20,7 @@ export default function LoginScreen({ navigation }) {
     <ScreenWrapper>
       <KeyboardAvoidingView
         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-        style={{ flex: 1 }} 
+        style={{ flex: 1 }}
         keyboardVerticalOffset={Platform.OS === 'ios' ? 64 : 0}
       >
         <Animated.View style={[styles.container, { opacity: fadeAnim }]}>
@@ -71,4 +71,70 @@ export default function LoginScreen({ navigation }) {
   );
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  logoContainer: {
+    alignItems: 'center',
+    marginBottom: 10, 
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    resizeMode: 'contain',
+  },
+  coloredContainer: {
+    padding: 20,
+    backgroundColor: '#F7F8FA', 
+    borderRadius: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.2,
+    shadowRadius: 1,
+    elevation: 1,
+  },
+  formContainer: {
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: '#424242',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  input: {
+    width: '100%',
+    paddingVertical: 12,
+    paddingHorizontal: 15,
+    borderWidth: 1,
+    borderColor: '#E0E0E0',
+    borderRadius: 10,
+    backgroundColor: '#FAFAFA',
+    marginBottom: 15,
+    fontSize: 16,
+  },
+  forgotPassword: {
+    color: '#007BFF',
+    textAlign: 'right',
+    marginBottom: 20,
+    fontWeight: '500',
+    fontSize: 14,
+  },
+  signupWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 10,
+  },
+  signupText: {
+    fontSize: 14,
+    color: '#757575',
+  },
+  signupLink: {
+    fontSize: 14,
+    color: '#007BFF',
+    fontWeight: '600',
+  },
+});
