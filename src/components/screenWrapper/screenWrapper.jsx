9f6dabf66797022066c6afab8bd84fb34728b9c1
Berro-1 <hussienberro1@gmail.com>
2024-08-24 13:00:58 +0300

    Created screen wrapper file

---
+++ src/components/screenWrapper/screenWrapper.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+export default function ScreenWrapper({ children, style }) {
+  return <View style={[styles.container, style]}>{children}</View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 40,
+    backgroundColor: '#FFFFFF',
+  },
+});
