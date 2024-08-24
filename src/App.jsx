ed3b299739006f1f3381f6a44ec0297f6183c360
Berro-1 <hussienberro1@gmail.com>
2024-08-24 17:30:22 +0300

    Added provider to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
+
 import OnboardingNavigator from './components/navigation/onboardingNavigator';
-// import LoginScreen from './screens/LoginScreen';
-// import SignupScreen from './screens/SignupScreen';
-  import MainStackNavigator from './components/navigation/mainStackNavigator'; // Assuming you have a main app navigator
+import MainStackNavigator from './components/navigation/mainStackNavigator'; // Assuming you have a main app navigator
 import LoginScreen from './screens/auth/loginScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Onboarding">
-        <Stack.Screen
-          name="Onboarding"
-          component={OnboardingNavigator}
-          options={{ headerShown: false }}
-        />
-         <Stack.Screen
-        name="Login"
-        component={LoginScreen} 
-        options={{ headerShown: false }}
-      />
-    
-        <Stack.Screen
-          name="Main"
-          component={MainStackNavigator}
-          options={{ headerShown: false }}
-        />
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Onboarding">
+          <Stack.Screen
+            name="Onboarding"
+            component={OnboardingNavigator}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{headerShown: false}}
+          />
 
-      </Stack.Navigator>
-    </NavigationContainer>
+          <Stack.Screen
+            name="Main"
+            component={MainStackNavigator}
+            options={{headerShown: false}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
