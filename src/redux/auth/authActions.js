7f9494845d1ccfa033f3bfa29a0b66e846ae49a5
Berro-1 <hussienberro1@gmail.com>
2024-08-25 17:24:52 +0300

    Added validate token action

--- src/redux/auth/authActions.js
+++ src/redux/auth/authActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {BACKEND_URL} from '@env'
+import { BACKEND_URL } from '@env';
 import { loginStart, loginSuccess, loginFailure, signupStart, signupSuccess, signupFailure } from './authSlice';
 
 // Login action creator
@@ -9,13 +9,11 @@ export const loginUser = (userData) => async (dispatch) => {
   try {
     const response = await axios.post(`${BACKEND_URL}/api/auth/login`, userData);
     dispatch(loginSuccess(response.data));
-    console.log('Login Successful:', response.data);
-
     await AsyncStorage.setItem('token', response?.data.token);
-
+    return true;
   } catch (error) {
-    console.error('Login Error:', error);
     dispatch(loginFailure(error.response?.data?.message || error.message || "Login failed"));
+    return false;
   }
 };
 
@@ -23,10 +21,26 @@ export const loginUser = (userData) => async (dispatch) => {
 export const signupUser = (userData) => async (dispatch) => {
   dispatch(signupStart());
   try {
-    const response = await axios.post(`${process.env.BACKEND_URL}/api/auth/register`, userData);
+    const response = await axios.post(`${BACKEND_URL}/api/auth/signup`, userData);
     dispatch(signupSuccess(response.data));
     await AsyncStorage.setItem('token', response?.data.token);
   } catch (error) {
     dispatch(signupFailure(error.response?.data?.message || error.message || "Signup failed"));
+  }
+};
+
+export const validateToken = () => async (dispatch) => {
+  const token = await AsyncStorage.getItem('token');
+  if (!token) return false;
+
+  try {
+    const response = await axios.get(`${BACKEND_URL}/api/auth/validate`, {
+      headers: { Authorization: `Bearer ${token}` }
+    });
+    dispatch(loginSuccess(response.data));
+    return true;
+  } catch (error) {
+    dispatch(loginFailure("Invalid token or session expired."));
+    return false;
   }
 };
