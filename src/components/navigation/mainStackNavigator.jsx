33089c5d19412895724f53757a2552fbee1c39c7
Berro-1 <hussienberro1@gmail.com>
2024-08-28 02:28:50 +0300

    Added recording screen in stack navigator

--- src/components/navigation/mainStackNavigator.jsx
+++ src/components/navigation/mainStackNavigator.jsx
@@ -3,24 +3,32 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../../screens/home/home';
 import DetailsScreen from '../../screens/details';
 import CustomHeader from '../../components/customHeader/customHeader';
+import RecordingScreen from '../../screens/recording/recording';
 
 const Stack = createNativeStackNavigator();
 
 export default function MainStackNavigator() {
   return (
-    <Stack.Navigator initialRouteName="Home">
+    <Stack.Navigator initialRouteName="Recording">
       <Stack.Screen
         name="Home"
         component={HomeScreen}
         options={{
-          header: () => <CustomHeader title="Home" />, // Use the custom header
+          header: () => <CustomHeader title="Home" />,
         }} 
       />
       <Stack.Screen
         name="Details"
         component={DetailsScreen}
         options={{
-          header: () => <CustomHeader title="Details" />, // Use the custom header for Details as well
+          header: () => <CustomHeader title="Details" />,
+        }}
+      />
+       <Stack.Screen
+        name="Recording"
+        component={RecordingScreen}
+        options={{
+          header: () => <CustomHeader title="Recording Room" />,
         }}
       />
     </Stack.Navigator>
