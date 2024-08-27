c4909f5a2179b6cabf1b9220134b9cbbfb1eba3c
Berro-1 <hussienberro1@gmail.com>
2024-08-28 02:29:11 +0300

    Added recording reducer in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+
 import authReducer from './auth/authSlice';
+import recordingReducer from './recording/recordingSlice';
 export const store = configureStore({
   reducer: {
   user: authReducer,
+  recordings: recordingReducer
   },
 });
 
