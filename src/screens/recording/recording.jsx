9067e0b7d94c1bd3ded54ffcbf9ef9aefd6c202d
Berro-1 <hussienberro1@gmail.com>
2024-08-28 02:26:13 +0300

    Added function to request permissions

--- src/screens/recording/recording.jsx
+++ src/screens/recording/recording.jsx
@@ -17,7 +17,77 @@ import {uploadRecording} from '../../redux/recording/RecordingActions';
 const audioRecorderPlayer = new AudioRecorderPlayer();
 
 export default function RecordingScreen({navigation}) {
-  
+  const [isRecording, setIsRecording] = useState(false);
+  const [dots, setDots] = useState('');
+  const [recordSecs, setRecordSecs] = useState(0);
+  const [formData, setFormData] = useState(null);
+  const dispatch = useDispatch();
+
+  // Access user data from Redux store
+  const user = useSelector(state => state.user.user);
+  const userId = user ? user._id : null;
+  const babyId = user ? user.babies[0]._id : null;
+
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      requestPermissions();
+    }
+  }, []);
+
+  const requestPermissions = async () => {
+    try {
+      const granted = await PermissionsAndroid.requestMultiple([
+        PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+        PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+        PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+      ]);
+
+      const writeGranted =
+        granted['android.permission.WRITE_EXTERNAL_STORAGE'] ===
+        PermissionsAndroid.RESULTS.GRANTED;
+      const readGranted =
+        granted['android.permission.READ_EXTERNAL_STORAGE'] ===
+        PermissionsAndroid.RESULTS.GRANTED;
+      const recordGranted =
+        granted['android.permission.RECORD_AUDIO'] ===
+        PermissionsAndroid.RESULTS.GRANTED;
+
+      if (!writeGranted || !readGranted || !recordGranted) {
+        Alert.alert(
+          'Permissions Required',
+          'All permissions need to be granted to use this feature.',
+        );
+      }
+    } catch (err) {
+      console.warn(err);
+      Alert.alert('Error', 'An error occurred while requesting permissions.');
+    }
+  };
+
+
+  useEffect(() => {
+    let interval;
+    if (isRecording) {
+      interval = setInterval(() => {
+        setDots(prev => (prev.length < 3 ? prev + '.' : ''));
+      }, 500);
+    } else {
+      setDots('');
+    }
+    return () => clearInterval(interval);
+  }, [isRecording]);
+
+  // Log formData whenever it changes
+  useEffect(() => {
+    if (formData) {
+      console.log('Form Data Updated:', formData);
+
+      // Dispatch uploadRecording action
+      dispatch(uploadRecording(formData));
+    }
+  }, [formData, dispatch]);
+
+  console.log('user', user, user.babies);
   return (
     <ScreenWrapper>
       <View style={styles.container}>
