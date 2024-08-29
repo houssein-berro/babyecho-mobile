11288f1e9b4f8fe440dbe453c011d140af9f3b68
Berro-1 <hussienberro1@gmail.com>
2024-08-29 14:15:32 +0300

    Fixed navigation logic

--- src/screens/auth/signupScreen.jsx
+++ src/screens/auth/signupScreen.jsx
@@ -38,7 +38,7 @@ export default function SignupScreen({ navigation }) {
 
   useEffect(() => {
     if (user) {
-      navigation.replace('Main');
+      navigation.replace('AddBaby'); // Navigate to AddBaby screen
     }
   }, [user, navigation]);
 
