7ccda94481987b1636a9603bb293f9e48d4d3bf3
Berro-1 <hussienberro1@gmail.com>
2024-08-29 16:40:03 +0300

    Added action addBabyToUser

--- src/redux/babies/babyActions.js
+++ src/redux/babies/babyActions.js
@@ -2,5 +2,13 @@ import axios from 'axios';
 import { addBabyStart, addBabySuccess, addBabyFailure } from './babySlice';
 import { BACKEND_URL } from '@env';
 
-
+export const addBabyToUser = (userId, babyData) => async (dispatch) => {    
+    dispatch(addBabyStart());
+    try {
+      const response = await axios.post(`${BACKEND_URL}/api/users/add-baby/${userId}`, babyData);
+      dispatch(addBabySuccess(response.data));
+    } catch (error) {
+      dispatch(addBabyFailure(error.response?.data?.message || error.message || "Failed to add baby"));
+    }
+  };
   
