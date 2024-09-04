668196cf57afa869a2689a5ac219823d90a596b8
Berro-1 <hussienberro1@gmail.com>
2024-09-05 00:14:42 +0300

    Added logic and variables

--- src/screens/devices/devicesScreen.jsx
+++ src/screens/devices/devicesScreen.jsx
@@ -11,3 +11,16 @@ import {useSelector} from 'react-redux';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import CustomHeader from '../../components/customHeader/customHeader';
 import ScreenWrapper from '../../components/screenWrapper/screenWrapper';
+
+export default function ConnectedDevicesScreen({navigation}) {
+  const [devices, setDevices] = useState([]);
+  const [searchText, setSearchText] = useState('');
+  const user = useSelector(state => state.user.user);
+
+  useEffect(() => {
+    // Fetch user's connected devices from the backend or state
+    const fetchedDevices = user?.devices || []; // Replace with actual data fetching logic
+    setDevices(fetchedDevices);
+  }, [user]);
+
+ 
