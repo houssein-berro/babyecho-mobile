b3f7bb81746b4d548d7d718fca46eca00a803742
Berro-1 <hussienberro1@gmail.com>
2024-09-05 01:08:54 +0300

    Adjusted logic

--- src/redux/recording/RecordingActions.js
+++ src/redux/recording/RecordingActions.js
@@ -6,23 +6,19 @@ export const uploadRecording = (formData) => async (dispatch) => {
   dispatch(uploadStart());
 
   try {
-    // Upload the recording to the backend
     const response = await axios.post(`${BACKEND_URL}/api/recordings`, formData, {
       headers: {
         'Content-Type': 'multipart/form-data',
       },
     });
     const { recordingId } = response.data; 
-    // Accessing the "audioFile" field
     const audioFileField = formData._parts.find(part => part[0] === 'audioFile');
 
     if (!audioFileField) {
       throw new Error('audioFile field not found in formData');
     }
 
-    const audioFile = audioFileField[1];  // Get the audio file object
-
-    // Create a new FormData for the ML server
+    const audioFile = audioFileField[1];  
     const mlFormData = new FormData();
     mlFormData.append('file', {
       uri: audioFile.uri,
@@ -30,7 +26,6 @@ export const uploadRecording = (formData) => async (dispatch) => {
       type: audioFile.type,
     });
 
-    // Send the file to the ML server
     const MLresponse = await axios.post(`${ML_URL}/upload`, mlFormData, {
       headers: {
         'Content-Type': 'multipart/form-data',
@@ -39,12 +34,10 @@ export const uploadRecording = (formData) => async (dispatch) => {
     await axios.put(`${BACKEND_URL}/api/recordings/${recordingId}/analysis`, {
       resultDetails: MLresponse.data.prediction,
     });
-    // Dispatch success action with the recording data and prediction from ML server
 
     dispatch(uploadSuccess({ mlPrediction: MLresponse?.data?.prediction }));
     return MLresponse.data.prediction;  
   } catch (error) {
-    // console.error('Error uploading recording:', error);
     dispatch(uploadFailure(error.message));
   }
 };
