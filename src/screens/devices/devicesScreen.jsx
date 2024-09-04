4bf8eb58117b5333ce047fa1f526f8235893ed3d
Berro-1 <hussienberro1@gmail.com>
2024-09-05 00:14:26 +0300

    Added imports

--- src/screens/devices/devicesScreen.jsx
+++ src/screens/devices/devicesScreen.jsx
@@ -1,1 +1,13 @@
- 
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  TextInput,
+  FlatList,
+} from 'react-native';
+import {useSelector} from 'react-redux';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import CustomHeader from '../../components/customHeader/customHeader';
+import ScreenWrapper from '../../components/screenWrapper/screenWrapper';
