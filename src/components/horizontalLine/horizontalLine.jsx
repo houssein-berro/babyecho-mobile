868fbd3a27457a810e8093feb5612ad1cc272a43
Berro-1 <hussienberro1@gmail.com>
2024-09-05 15:34:45 +0300

    Added layout

---
+++ src/components/horizontalLine/horizontalLine.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const HorizontalLine = ({ style }) => {
+  return <View style={[styles.line, style]} />;
+};
+
+
+export default HorizontalLine;
