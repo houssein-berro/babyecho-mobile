98cee7210bfe67510d2e5f1704d9fc3c88c10905
Berro-1 <hussienberro1@gmail.com>
2024-09-13 15:41:05 +0300

    Added devices reducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import authReducer from './auth/authSlice';
 import recordingReducer from './recording/recordingSlice';
+import deviceReducer from './devices/devicesSlice';
 export const store = configureStore({
   reducer: {
   user: authReducer,
-  recordings: recordingReducer
+  recordings: recordingReducer,
+  devices: deviceReducer
   },
 });
 
