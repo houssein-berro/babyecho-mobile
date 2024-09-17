a9deed004bb12080ce03efb6d46a33f7a0a00cb0
Berro-1 <hussienberro1@gmail.com>
2024-09-17 08:20:12 +0300

    Added baby reducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,11 +3,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './auth/authSlice';
 import recordingReducer from './recording/recordingSlice';
 import deviceReducer from './devices/devicesSlice';
+import babyReducer from './babies/babySlice';
 export const store = configureStore({
   reducer: {
   user: authReducer,
   recordings: recordingReducer,
-  devices: deviceReducer
+  devices: deviceReducer,
+  baby: babyReducer,
   },
 });
 
